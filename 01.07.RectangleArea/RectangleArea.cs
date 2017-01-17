85eddcfa9c797ae540371d05596f5e009cc2dff9
Georgi Spasov <georgi.spasov@outlook.com>
2017-01-18 01:48:09 +0200

    Rectangle Area

---
+++ 01.07.RectangleArea/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._07.RectangleArea
+{
+    class RectangleArea
+    {
+        static void Main(string[] args)
+        {
+            var a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine(a * b);
+        }
+    }
+}
