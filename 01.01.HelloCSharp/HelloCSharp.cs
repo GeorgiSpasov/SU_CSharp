fe2b9e26b5db3fd4668b6143dcc89640efaf181b
Georgi Spasov <georgi.spasov@outlook.com>
2017-01-18 01:24:00 +0200

    SoftUni C#

---
+++ 01.01.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._01.HelloCSharp
+{
+    class HelloCSharp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+        }
+    }
+}
