e6ed99f94f8585eb1885956e3b1be5b092db7a40
Georgi Spasov <georgi.spasov@outlook.com>
2017-01-18 01:53:30 +0200

    Square of Stars

---
+++ 01.08.SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._08.SquareOfStars
+{
+    class SquareOfStars
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new String('*', n));
+            for (int i = 1; i < n - 1; i++)
+            {
+                Console.WriteLine("*" + new string(' ', n - 2) + "*");
+            }
+            Console.WriteLine(new String('*', n));
+        }
+    }
+}
