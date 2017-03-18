b6d22450f8a7a2379b4268c0a978d9e5c43697f0
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 19:23:22 +0200

    Circle Area and Perimeter

---
+++ CircleAreaAndPerimeter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse(Console.ReadLine());
+            Console.WriteLine("Area = " + Math.PI * Math.Pow(r, 2));
+            Console.WriteLine("Perimeter = " + 2 * Math.PI * r);
+        }
+    }
+}
