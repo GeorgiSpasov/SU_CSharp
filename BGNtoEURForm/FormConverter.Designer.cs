130d68c9c5fffe750ade3beb96b3cebd0c80afed
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 21:39:37 +0200

    Form Converter

---
+++ BGNtoEURForm/FormConverter.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BGNtoEURForm
+{
+    partial class FormConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblConvert = new System.Windows.Forms.Label();
+            this.lblCurrency = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(93, 11);
+            this.numericUpDownAmount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(102, 26);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            this.numericUpDownAmount.KeyUp += new System.Windows.Forms.KeyEventHandler(this.numericUpDownAmount_KeyUp);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.Color.PaleGreen;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblResult.Location = new System.Drawing.Point(18, 48);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(287, 29);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblConvert
+            // 
+            this.lblConvert.AutoSize = true;
+            this.lblConvert.Location = new System.Drawing.Point(18, 14);
+            this.lblConvert.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConvert.Name = "lblConvert";
+            this.lblConvert.Size = new System.Drawing.Size(64, 20);
+            this.lblConvert.TabIndex = 2;
+            this.lblConvert.Text = "Convert";
+            // 
+            // lblCurrency
+            // 
+            this.lblCurrency.AutoSize = true;
+            this.lblCurrency.Location = new System.Drawing.Point(204, 14);
+            this.lblCurrency.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCurrency.Name = "lblCurrency";
+            this.lblCurrency.Size = new System.Drawing.Size(101, 20);
+            this.lblCurrency.TabIndex = 3;
+            this.lblCurrency.Text = "BGN to USD";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 101);
+            this.Controls.Add(this.lblCurrency);
+            this.Controls.Add(this.lblConvert);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BGN to USD";
+            this.Load += new System.EventHandler(this.FormConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblConvert;
+        private System.Windows.Forms.Label lblCurrency;
+    }
+}
+
---
+++ BGNtoEURForm/FormConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BGNtoEURForm
+{
+    public partial class FormConverter : Form
+    {
+        public FormConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void FormConverter_Load(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void numericUpDownAmount_ValueChanged(object sender, EventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void numericUpDownAmount_KeyUp(object sender, KeyEventArgs e)
+        {
+            ConvertCurrency();
+        }
+
+        private void ConvertCurrency()
+
+        {
+            var amountBGN = this.numericUpDownAmount.Value;
+            var amountEUR = amountBGN * 1.95583m;
+            this.lblResult.Text = amountBGN + " BGN = " +             Math.Round(amountEUR, 2) + " EUR";
+        }
+    }
+}
