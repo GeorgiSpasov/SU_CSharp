7c9bde188c8edb9c3c6e8c6929e60c1720c37ba1
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-19 00:05:42 +0200

    Sum Seconds

---
+++ SumSeconds/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            var sec = a + b + c;
+            var min = 0;
+
+            while (sec >= 60)
+            {
+                min++;
+                sec -= 60;
+            }
+
+            Console.WriteLine(string.Format("{0}:{1}", min, sec.ToString().PadLeft(2, '0')));
+        }
+    }
+}
