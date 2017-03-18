40ebd801c910633e7c3011f451e9399c609fed6d
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-19 00:13:55 +0200

    Password Guess

---
+++ PasswordGuess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(input == "s3cr3t!P@ssw0rd" ? "Welcome" : "Wrong Password!");
+        }
+    }
+}
