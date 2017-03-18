608462d91ab1b0259ae5f7ff8c1f953d03ddee61
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-19 01:07:09 +0200

    Currency Converter App

---
+++ CurrencyConverterApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CurrencyConverterApp
+{
+    partial class FormConverter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDownAmount = new System.Windows.Forms.NumericUpDown();
+            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.labelBGN = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDownAmount
+            // 
+            this.numericUpDownAmount.DecimalPlaces = 2;
+            this.numericUpDownAmount.Location = new System.Drawing.Point(14, 18);
+            this.numericUpDownAmount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDownAmount.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.Name = "numericUpDownAmount";
+            this.numericUpDownAmount.Size = new System.Drawing.Size(152, 26);
+            this.numericUpDownAmount.TabIndex = 0;
+            this.numericUpDownAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDownAmount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownAmount.ValueChanged += new System.EventHandler(this.numericUpDownAmount_ValueChanged);
+            // 
+            // comboBoxCurrency
+            // 
+            this.comboBoxCurrency.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCurrency.FormattingEnabled = true;
+            this.comboBoxCurrency.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "GBP"});
+            this.comboBoxCurrency.Location = new System.Drawing.Point(244, 18);
+            this.comboBoxCurrency.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxCurrency.Name = "comboBoxCurrency";
+            this.comboBoxCurrency.Size = new System.Drawing.Size(75, 28);
+            this.comboBoxCurrency.TabIndex = 1;
+            this.comboBoxCurrency.SelectedIndexChanged += new System.EventHandler(this.comboBoxCurrency_SelectedIndexChanged);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BackColor = System.Drawing.Color.LightGreen;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResult.Location = new System.Drawing.Point(13, 62);
+            this.labelResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(306, 37);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelBGN
+            // 
+            this.labelBGN.AutoSize = true;
+            this.labelBGN.Location = new System.Drawing.Point(174, 21);
+            this.labelBGN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelBGN.Name = "labelBGN";
+            this.labelBGN.Size = new System.Drawing.Size(62, 20);
+            this.labelBGN.TabIndex = 3;
+            this.labelBGN.Text = "BGN ->";
+            // 
+            // FormConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 114);
+            this.Controls.Add(this.labelBGN);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.comboBoxCurrency);
+            this.Controls.Add(this.numericUpDownAmount);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConverter";
+            this.Text = "Currency Converter";
+            this.Load += new System.EventHandler(this.FormConverter_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAmount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDownAmount;
+        private System.Windows.Forms.ComboBox comboBoxCurrency;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label labelBGN;
+    }
+}
+
