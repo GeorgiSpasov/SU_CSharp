61f0fa24c39ea3ef44b688fbe9be55f642ee5d56
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-19 00:28:14 +0200

    Number 100...200

---
+++ NumberBetween/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberBetween
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            if (num >= 100)
+            {
+                Console.WriteLine(num <= 200 ? "Between 100 and 200" : "Greater than 200");
+            }
+            else
+            {
+                Console.WriteLine("Less than 100");
+            }
+        }
+    }
+}
