91a7fdf34beab6b05e347cf278cba51a580e0cf3
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 22:36:18 +0200

    Excellent Result

---
+++ ExcellentResult/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExcellentResult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = double.Parse(Console.ReadLine());
+            if (result >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
