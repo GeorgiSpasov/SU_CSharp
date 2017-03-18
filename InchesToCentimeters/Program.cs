499fcdd85dd3a1fd0e98ec4bdce5f230d5ac6a8c
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 19:02:50 +0200

    Inches to Centimeters

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("inches = ");
+            var inches = double.Parse(Console.ReadLine());
+            Console.WriteLine("Centimeters = {0}", inches * 2.54);
+        }
+    }
+}
