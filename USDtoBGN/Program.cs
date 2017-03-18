89e3ed578790e678fcde982c9b3cf5c512ea923d
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 19:43:47 +0200

    USD to BGN

---
+++ USDtoBGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usd = double.Parse(Console.ReadLine());
+            var bgn = usd * 1.79549;
+            Console.WriteLine(Math.Round(bgn,2) + " USD");
+        }
+    }
+}
