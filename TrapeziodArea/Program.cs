b149a5391ad5a04102c26dfde185bb7beb3b77f2
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 19:19:36 +0200

    Trapeziod Area

---
+++ TrapeziodArea/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TrapeziodArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Trapezoid area = " + (a + b) * h / 2);
+
+        }
+    }
+}
