3acec267419bc375ac479f5ba5945b9c862dd004
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 20:29:14 +0200

    1000 Days After Birth

---
+++ DaysAfterBirth/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace DaysAfterBirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            var format = "dd-MM-yyyy";
+            var inputDate = DateTime.ParseExact(Console.ReadLine(),format, provider);
+            var newDate = inputDate.AddDays(999);
+            Console.WriteLine(newDate.ToString(format));
+            
+        }
+    }
+}
