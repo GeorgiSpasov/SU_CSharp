d1309d9dc87b58d1c3bba66919ccae0af2b27141
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 22:38:56 +0200

    Excellent or Not

---
+++ ExcellentOrNot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExcellentOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = double.Parse(Console.ReadLine());
+            if (result >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else
+            {
+                Console.WriteLine("Not Excellent.");
+            }
+        }
+    }
+}
