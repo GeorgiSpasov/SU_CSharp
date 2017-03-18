5b684a647c30b05e21c9f7a6c5e0344afaadf3f0
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-19 00:32:26 +0200

    Equal Words

---
+++ EqualWords/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EqualWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine().ToLower();
+            var second = Console.ReadLine().ToLower();
+
+            Console.WriteLine(first == second ? "yes" : "no");
+        }
+    }
+}
