f4669c88dab12804f6ca25b3c831e3319948868e
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 18:13:24 +0200

    Sumator

---
+++ Sumator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Sumator
+{
+    partial class FormCalculate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.lblPlus = new System.Windows.Forms.Label();
+            this.lblEquals = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(12, 12);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(117, 20);
+            this.txtBox1.TabIndex = 0;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(155, 11);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(117, 20);
+            this.txtBox2.TabIndex = 1;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(297, 12);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(117, 20);
+            this.txtSum.TabIndex = 2;
+            // 
+            // lblPlus
+            // 
+            this.lblPlus.AutoSize = true;
+            this.lblPlus.Location = new System.Drawing.Point(136, 18);
+            this.lblPlus.Name = "lblPlus";
+            this.lblPlus.Size = new System.Drawing.Size(13, 13);
+            this.lblPlus.TabIndex = 3;
+            this.lblPlus.Text = "+";
+            // 
+            // lblEquals
+            // 
+            this.lblEquals.AutoSize = true;
+            this.lblEquals.Location = new System.Drawing.Point(278, 15);
+            this.lblEquals.Name = "lblEquals";
+            this.lblEquals.Size = new System.Drawing.Size(13, 13);
+            this.lblEquals.TabIndex = 4;
+            this.lblEquals.Text = "=";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 48);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(402, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // FormCalculate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 85);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblEquals);
+            this.Controls.Add(this.lblPlus);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Name = "FormCalculate";
+            this.Text = "Sumator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Label lblEquals;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblPlus;
+    }
+}
+
---
+++ Sumator/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Sumator
+{
+    public partial class FormCalculate : Form
+    {
+        public FormCalculate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var num1 = decimal.Parse(this.txtBox1.Text);
+                var num2 = decimal.Parse(this.txtBox2.Text);
+                var sum = num1 + num2;
+                txtSum.Text = sum.ToString();
+            }
+            catch (Exception)
+            {
+                txtSum.Text = "Error";
+            }
+
+        }
+    }
+}
