ed963f3ba7cbe3cd9e16edf767ae477743c283fe
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 19:35:53 +0200

    Celsius to Fahrenheit

---
+++ CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = double.Parse(Console.ReadLine());
+            var f = Math.Round((c * 1.8 + 32), 2);
+            Console.WriteLine(f);
+        }
+    }
+}
