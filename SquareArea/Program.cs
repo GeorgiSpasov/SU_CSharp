753d50a63fa2d5e22325b8cf1f2c78d3a8f23a30
Georgi Spasov <georgi.spasov@outlook.com>
2017-03-18 18:50:50 +0200

    Square Area

---
+++ SquareArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            var a = int.Parse(Console.ReadLine());
+            var area = Math.Pow(a, 2);
+            Console.WriteLine("Square = {0}", area);
+        }
+    }
+}
