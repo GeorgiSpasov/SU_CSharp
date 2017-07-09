548917e5b635cf4b07ab408ae2ff94a8ccdfc5cf
Georgi Spasov <georgi.spasov@outlook.com>
2017-07-09 18:29:33 +0300

    Poke Mon

---
+++ 01.PokeMon/PokeMon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.PokeMon
+{
+    class PokeMon
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int originalPower = power;
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int targets = 0;
+
+            while (power >= distance)
+            {
+                if (exhaustionFactor != 0 && (double)power == (double)originalPower / 2.0)
+                {
+                    power /= exhaustionFactor;
+                }
+                if (power < distance)
+                {
+                    break;
+                }
+                power = power - distance;
+                targets++;
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(targets);
+        }
+    }
+}
